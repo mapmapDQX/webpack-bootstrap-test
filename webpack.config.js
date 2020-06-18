116b48b25466e4b177a139d3afa7e073e6fb91fd
まぷまぷ@DQX <mapmapdqx@gmail.com>
2020-06-18 13:05:29 +0900

    webpack-dev-serverを簡単に使える設定を追加

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,12 @@ module.exports = {
           ]
         }
       ]
+    },
+
+    // ローカル開発用環境を立ち上げる
+    // 実行時にブラウザが自動的に localhost を開く
+    devServer: {
+      contentBase: "./",
+      open: true
     }
-  };
-  
+};
